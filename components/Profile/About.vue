6b81a97ee133fd61f7ec6e6c04a494eec1a354b0
MahardikaKDenie <dikamahar884@gmail.com>
2022-07-26 13:51:00 +0700

    p

--- components/Profile/About.vue
+++ components/Profile/About.vue
@@ -1,16 +1,74 @@
 <template>
-  <div class="w-full p-36 bg-red-900">
+  <div class="w-full p-36 bg-red-800">
     <div class="grid grid-rows-1">
-      <div class="grid grid-cols-3 gap-4">
+      <div class="grid grid-cols-3">
         <div>
-          <h1 class="text-5xl font-bold text-white">Visi</h1>
-          <p>
+          <span class="text-5xl text-center font-bold text-white border-b-2"
+            >Visi</span
+          >
+          <p class="my-2 subpixel-antialiased text-lg text-white p-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
             doloribus laborum eum voluptate consequuntur in obcaecati non
             sapiente, voluptates vitae optio maxime dolorem culpa autem totam,
             voluptatibus nesciunt, sequi illo.
           </p>
         </div>
+        <div class="flex justify-center p-32 items-center">
+          <div
+            class="border-2 position-relative p-28 rounded-full items-center bg-contain bg-no-repeat"
+            style="background-image: url(himatif.png)"
+          ></div>
+        </div>
+        <div>
+          <span class="text-5xl text-center font-bold text-white border-b-2"
+            >Misi</span
+          >
+          <p class="my-2 subpixel-antialiased text-lg text-white p-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            doloribus laborum eum voluptate consequuntur in obcaecati non
+            sapiente, voluptates vitae optio maxime dolorem culpa autem totam,
+            voluptatibus nesciunt, sequi illo.
+          </p>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <div class="grid grid-row-1">
+      <div class="grid grid-cols-3">
+        <div>
+          <span class="text-5xl text-center font-bold text-white border-b-2"
+            >Sejarah</span
+          >
+          <p class="my-2 subpixel-antialiased text-lg text-white p-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            doloribus laborum eum voluptate consequuntur in obcaecati non
+            sapiente, voluptates vitae optio maxime dolorem culpa autem totam,
+            voluptatibus nesciunt, sequi illo.
+          </p>
+        </div>
+        <div></div>
+        <div>
+          <span class="text-5xl text-center font-bold text-white border-b-2"
+            >Makna Lambang</span
+          >
+          <p class="my-2 subpixel-antialiased text-lg text-white p-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            doloribus laborum eum voluptate consequuntur in obcaecati non
+            sapiente, voluptates vitae optio maxime dolorem culpa autem totam,
+            voluptatibus nesciunt, sequi illo.
+          </p>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <div class="container my-12">
+      <div class="justify-center flex my-6">
+        <span class="text-5xl text-center text-white font-bold">
+          Struktur organisasi
+        </span>
+      </div>
+      <div class="flex justify-center">
+        <img src="/struktur.png" alt="" />
       </div>
     </div>
   </div>
